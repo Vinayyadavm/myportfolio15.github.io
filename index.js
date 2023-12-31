ad496a033c98ce69f2014598e7092ddbdbe00880
Vinay Yadav <123170049+Vinayyadavm@users.noreply.github.com>
2023-12-31 04:01:41 -0500

    index.js

--- index.js
+++ index.js
@@ -3,21 +3,13 @@ document.addEventListener("DOMContentLoaded", function () {
     var scrollUpBtn = document.querySelector('.scroll-up-btn');
     var menuBtn = document.querySelector('.menu-btn');
     var menuItems = document.querySelectorAll('.navbar .menu li a');
-    
+
     window.addEventListener("scroll", function () {
         // Sticky navbar on scroll
-        if (window.scrollY > 20) {
-            navbar.classList.add("sticky");
-        } else {
-            navbar.classList.remove("sticky");
-        }
+        navbar.classList.toggle("sticky", window.scrollY > 20);
 
         // Scroll-up button show/hide
-        if (window.scrollY > 500) {
-            scrollUpBtn.classList.add("show");
-        } else {
-            scrollUpBtn.classList.remove("show");
-        }
+        scrollUpBtn.classList.toggle("show", window.scrollY > 500);
     });
 
     // Slide-up script
@@ -41,54 +33,47 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     // Typing text animation script
-    var typeAnimation = function (element, strings) {
+    function typeAnimation(element, strings) {
         var currentStringIndex = 0;
-        var currentString = strings[currentStringIndex];
         var typingSpeed = 100;
         var backspacingSpeed = 60;
 
-        var type = function () {
+        function type() {
             if (currentStringIndex < strings.length) {
-                if (element.textContent !== currentString) {
-                    element.textContent += currentString.charAt(0);
+                if (element.textContent !== strings[currentStringIndex]) {
+                    element.textContent += strings[currentStringIndex].charAt(0);
                     setTimeout(type, typingSpeed);
                 } else {
                     setTimeout(backspace, 1000);
                 }
             }
-        };
+        }
 
-        var backspace = function () {
+        function backspace() {
             if (element.textContent.length > 0) {
                 element.textContent = element.textContent.slice(0, -1);
                 setTimeout(backspace, backspacingSpeed);
             } else {
                 currentStringIndex = (currentStringIndex + 1) % strings.length;
-                currentString = strings[currentStringIndex];
                 setTimeout(type, 500);
             }
-        };
+        }
 
         type();
-    };
+    }
 
     typeAnimation(document.querySelector(".typing"), ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"]);
     typeAnimation(document.querySelector(".typing-2"), ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"]);
 
     // Owl carousel script (Simplified, without jQuery)
     var carousel = document.querySelector('.carousel');
-    var items = document.querySelectorAll('.carousel .item');
-    var margin = 20;
     var currentIndex = 0;
 
     function updateCarousel() {
         var windowWidth = window.innerWidth;
+        var margin = windowWidth < 600 ? 20 : 40;
 
-        if (windowWidth < 600) {
-            carousel.style.transform = 'translateX(' + (-currentIndex * (items[0].offsetWidth + margin)) + 'px)';
-        } else {
-            carousel.style.transform = 'translateX(' + (-currentIndex * (items[0].offsetWidth + margin * 2)) + 'px)';
-        }
+        carousel.style.transform = 'translateX(' + (-currentIndex * (items[0].offsetWidth + margin)) + 'px)';
     }
 
     window.addEventListener('resize', updateCarousel);
